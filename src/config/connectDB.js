0380df0578a1988676bceff261bf3e787f0610f4
Nguyen Vu <nguyentuanvuk62@gmail.com>
2024-06-14 01:53:40 +0700

    config database on heroku

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,9 +1,17 @@
 const { Sequelize } = require('sequelize');
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('shopee', 'root', null, {
-    host: 'localhost',
-    dialect: 'mysql'
+const sequelize = new Sequelize('z0vz6mqi3014e6pl', 'f4b67auhz6s3gmt6',
+    "et3itnhm1vziys18", {
+    host: 'mwgmw3rs78pvwk4e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    dialect: 'mysql',
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 
 const connection = async () => {
